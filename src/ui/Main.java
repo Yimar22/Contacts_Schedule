dbde371a7a2e8d13d4b684629747b5c2ff2483cd
Yimar <yimar2001@gmail.com>
2020-01-29 17:40:28 -0500

    txt files

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,8 +1,25 @@
 package ui;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("balanceGeneral.fxml"));
+
+		Scene scene = new Scene(root);
+		stage.setTitle("Balance General");
+		stage.setResizable(false);
+		stage.setScene(scene);
+		stage.show();
+
 	}
 }
