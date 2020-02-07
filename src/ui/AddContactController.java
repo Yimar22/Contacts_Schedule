749e3584b693e124508c8b01840d9bd81b152e1e
Yimar22 <yimar2001@gmail.com>
2020-02-06 21:45:45 -0500

    Read is added when adding user}

--- src/ui/AddContactController.java
+++ src/ui/AddContactController.java
@@ -1,8 +1,10 @@
 package ui;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.net.MalformedURLException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,19 +12,15 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import model.Contact;
 
 public class AddContactController {
 
+	private String imgURL;
 	
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
     @FXML
     private TextField name;
 	
@@ -45,32 +43,32 @@ public class AddContactController {
     private ImageView photo;
 
     @FXML
-    void addPhoto(ActionEvent event) throws IOException {
+    void addPhoto(ActionEvent event) throws MalformedURLException{
     	//ask if it's a web or a local photo
     	//Boolean defaultPhoto = true;
-    	String path = "/Contacts_Schedule/documents/images/avatar-icon-vector-illustration.jpg";
-    	
-    	Parent root = FXMLLoader.load(getClass().getResource("addImage.fxml"));
-		Scene scene = new Scene(root);
-		Stage appStage = new Stage();
-		appStage.setScene(scene);
-		appStage.setTitle("Add Image");
-		appStage.toFront();
-		appStage.show();
-    	photo = new ImageView(path);
+    	Image img;
+    	FileChooser fc = new FileChooser();
+    	fc.setTitle("Choose an image");
+    	String defaultDirectory = new File("").getAbsolutePath();
+    	defaultDirectory+="\\documents\\images\\profileimgs";
+    	fc.setInitialDirectory(new File(defaultDirectory));
+    	imgURL = (fc.showOpenDialog(photo.getScene().getWindow()).toURI().toURL().toExternalForm());
+    	photo.setImage(new Image(imgURL));
+    	String entries[] = imgURL.split("/");
+    	imgURL = "Contacts_Schedule\\documents\\images\\profileimgs\\"+entries[entries.length-1];
     }
 
     @FXML
     void save(ActionEvent event) throws IOException {
     	//Check if the contact is already added
-    	 new Contact(name.getText(), lastName.getText(), Integer.parseInt(age.getText()), email.getText(), Integer.parseInt(phoneNumber.getText()), birthDate.getText(), photo.getAccessibleText());
-    	
+    	BufferedWriter bw = new BufferedWriter(new FileWriter(new File("").getAbsolutePath()+"\\documents\\data\\contacts.txt"));
+    	bw.write(name.getText()+";"+lastName.getText()+";"+age.getText()+";"+ email.getText()+";"+ phoneNumber.getText()+";" +birthDate.getText()+";"+imgURL+"\n");
     	//Write the new row of contacts' file
     	//Confirm the new contact was added
     	//Ask to the user wants to add another contact or not
+    	bw.close();
     	
-    	
-    	Parent root = FXMLLoader.load(getClass().getResource("contacts.fxml"));
+    	Parent root = FXMLLoader.load(getClass().getResource("contact.fxml"));
 		Scene scene = new Scene(root);
 		Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		appStage.setScene(scene);
--- src/ui/AddImageController.java
+++ src/ui/AddImageController.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BackgroundImage;
 
 public class AddImageController {
 	
