3bc3840364e56baedb2480992c996536a8120086
Juan Diego Naranjo <juannaranjo1206@gmail.com>
2020-02-08 11:18:01 -0500

    contacts and courses getters and setters added

--- src/model/Schedule.java
+++ src/model/Schedule.java
@@ -7,6 +7,17 @@ public class Schedule {
 	private ArrayList<Contact> contacts;
 	private ArrayList<Course> courses;
 	
-	
+	public ArrayList<Contact> getContacts() {
+		return contacts;
+	}
+	public void setContacts(ArrayList<Contact> contacts) {
+		this.contacts = contacts;
+	}
+	public ArrayList<Course> getCourses() {
+		return courses;
+	}
+	public void setCourses(ArrayList<Course> courses) {
+		this.courses = courses;
+	}
 	
 }
