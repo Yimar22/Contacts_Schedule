73cd01f3a40d2776a0a830ef30c5afaafd33c3ac
Yimar David Tamayo Hernandez <1006037731@academico.icesi.edu.co>
2020-02-07 21:07:52 -0500

    AddCourseUI and AddCourseCotroller add

--- src/model/Course.java
+++ src/model/Course.java
@@ -2,4 +2,42 @@ package model;
 
 public class Course {
 
+	private String name;
+	private int credits; 
+	private String teacherName;
+	private int NRC;
+	
+	
+	public Course(String name, int credits, String teacherName, int nRC) {
+		this.name = name;
+		this.credits = credits;
+		this.teacherName = teacherName;
+		NRC = nRC;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCredits() {
+		return credits;
+	}
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
+	public String getTeacherName() {
+		return teacherName;
+	}
+	public void setTeacherName(String teacherName) {
+		this.teacherName = teacherName;
+	}
+	public int getNRC() {
+		return NRC;
+	}
+	public void setNRC(int nRC) {
+		NRC = nRC;
+	}
+	
+	
 }
--- src/model/Schedule.java
+++ src/model/Schedule.java
@@ -1,5 +1,12 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Schedule {
 
+	private ArrayList<Contact> contacts;
+	private ArrayList<Course> courses;
+	
+	
+	
 }
